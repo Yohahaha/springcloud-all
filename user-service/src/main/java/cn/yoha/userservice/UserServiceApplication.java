ebf201f4420be75f587ad9fa58a992960c47377e
Yohahaha <714615435@qq.com>
2019-12-15 17:27:36 +0800

    将api-gateway增加到nacos中，并将其改造为nacos-config组件

--- user-service/src/main/java/cn/yoha/userservice/UserServiceApplication.java
+++ user-service/src/main/java/cn/yoha/userservice/UserServiceApplication.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableFeignClients
 public class UserServiceApplication {
     private static final Logger log = LoggerFactory.getLogger(UserServiceApplication.class);
 
